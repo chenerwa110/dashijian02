d063c19668f60b496705c5ad6fce7c05faf240c6
chen <itheima@itcast.cn>
2020-10-07 20:46:46 +0800

    登录和注册功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,7 @@
 //开发环境服务器地址
-var baseURl = "http://ajax.frontend.itheima.net";
+var baseURL = "http://ajax.frontend.itheima.net";
+//拦截所有ajax请求
 $.ajaxPrefilter(function (params) {
-  params.url = baseURl + params.url;
+  //拼接对应的服务器地址
+  params.url = baseURL + params.url;
 });
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,27 +1,27 @@
+//入口函数
 $(function () {
-  // 点击去注册账号，隐藏登录区域，显示去注册区域
+  //点击去注册账号,隐藏登录区域，显示注册区域
   $("#link-reg").on("click", function () {
     $(".login-box").hide();
     $(".reg-box").show();
   });
-  //点击去登录，隐藏去注册区域，显示登录区域
+  //点击去登录，隐藏注册区域，显示登录区域
   $("#link-login").on("click", function () {
     $(".reg-box").hide();
     $(".login-box").show();
   });
-  // 自定义校验规则
-  // 从layui中获取form对象
+  //自定义验证规则
   var form = layui.form;
   form.verify({
     //密码规则
     pwd: [/^[\S]{6,12}$/, "密码必须6到12位，且不能出现空格"],
-    //确定密码规则
+    //确认密码规则
     repwd: function (value) {
-      //获取name为password的标签
+      //获取密码框内容
       var pwd = $(".reg-box [name=password]").val();
-      //比较俩次输入密码是否一致
+      //判断确认密码框和密码框中的内容是否一致
       if (value !== pwd) {
-        return "俩次密码输入不一致！";
+        return "俩次密码不一致";
       }
     },
   });
@@ -32,7 +32,7 @@ $(function () {
     e.preventDefault();
     //发送ajax
     $.ajax({
-      method: "POST",
+      method: "post",
       url: "/api/reguser",
       data: {
         username: $(".reg-box [name=username]").val(),
@@ -44,7 +44,7 @@ $(function () {
           return layer.msg(res.message);
         }
         //提交成功后处理代码
-        layer.msg("注册成功，请登录");
+        layer.msg("注册成功，请登录！");
         //手动切换到登录表单
         $("#link-login").click();
         //重置form表单
@@ -58,19 +58,19 @@ $(function () {
     e.preventDefault();
     //发送ajax
     $.ajax({
-      method: "POST",
+      method: "post",
       url: "/api/login",
       data: $(this).serialize(),
       success: function (res) {
-        //校验返回状态
+        //返回状态判断
         if (res.status != 0) {
           return layer.msg(res.message);
         }
         //提示信息，保存token，跳转页面
-        layer.msg("登录成功");
-        //保存token，未来的的接口要使用token
+        layer.msg("恭喜你，登录成功！");
+        //保存token
         localStorage.setItem("token", res.token);
-        //跳转
+        //跳转页面
         location.href = "/index.html";
       },
     });
