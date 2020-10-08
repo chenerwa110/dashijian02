adf2eda17e9712ceb4a42c3779b79f86f2fb678d
chen <itheima@itcast.cn>
2020-10-08 20:45:47 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,4 +4,19 @@ var baseURL = "http://ajax.frontend.itheima.net";
 $.ajaxPrefilter(function (params) {
   //拼接对应的服务器地址
   params.url = baseURL + params.url;
+  if (params.url.indexOf("/my/") !== -1) {
+    params.headers = {
+      Authorization: localStorage.getItem("token") || "",
+    };
+  }
+  //拦截所有的响应，判断身份认证信息
+  params.complete = function (res) {
+    var obj = res.responseJSON;
+    if (obj.status == 1 && obj.message == "身份认证失败！") {
+      //清空本地token
+      localStorage.removeItem("token");
+      //页面跳转
+      location.href = "/login.html";
+    }
+  };
 });
---
+++ assets/js/index.js
@@ -0,0 +1,43 @@
+$(function () {
+  getUserInof();
+  //退出
+  var layer = layui.layer;
+  $("#btnLogout").on("click", function () {
+    layer.confirm("确定退出登录?", { icon: 3, title: "提示" }, function (
+      index
+    ) {
+      // 1. 清空本地存储中的 token
+      localStorage.removeItem("token");
+      // 2. 重新跳转到登录页面
+      location.href = "/login.html";
+      //框架提供的 关闭 confirm 询问框
+      layer.close(index);
+    });
+  });
+});
+function getUserInof() {
+  $.ajax({
+    url: "/my/userinfo",
+    // headers: {
+    //   Authorization: localStorage.getItem("token") || "",
+    // },
+    success: function (res) {
+      if (res.starus !== 0) {
+        return layui.layer.msg(res.message);
+      }
+      renderAvatar(res.data);
+    },
+  });
+}
+function renderAvatar(user) {
+  var name = user.nickname || user.username;
+  $("#welcome").html("欢迎&nbsp;&nbsp;" + name);
+  if (user.user_pic !== null) {
+    $(".layui-nav-img").show().attr("src", user.user_pic);
+    $(".user-avatar").hide();
+  } else {
+    $(".layui-nav-img").hide();
+    var text = name[0].toUpperCase();
+    $(".user-avatar").show().html(text);
+  }
+}
